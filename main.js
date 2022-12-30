412cfd46f0e550111e3cc08ccc087f35f5c3f525
bpa34 <brett.atkinson@me.com>
2022-12-30 11:38:27 -0800

    First Deploy of working colorpicker

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-let button = document.getElementById('color-button');
-let colorContainer = document.getElementById('color-container')
+// const colorContainer = document.getElementById('color-container');
 
 // This random number function will create color codes for the randomColor variable
 function colorValue() {
   return Math.floor(Math.random() * 256);
 }
 
-function colorChange(event){
-let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
-event.target.style.backgroundColor = randomColor
+function generateColor(){
+  let randomColor = 'rgb(' + colorValue() + ', ' + colorValue() + ', ' + colorValue() + ')';
+  return randomColor
 }
 
-button.onclick = colorChange;
+function changeBackgroundColor() {
+  const colorContainer = document.getElementById('color-container');
+  colorContainer.style.backgroundColor = generateColor();
+  document.getElementById('rgbCode').innerHTML = `${generateColor()}`
+  colorContainer.style.border = '5px white solid'
+  colorContainer.style.display = "flex"
+}
 
